e2ce6a4d07554fd8aa563ba3fc9a4c6894cd9a8c
davidcraccer <127027894+davidcraccer@users.noreply.github.com>
2023-03-22 19:12:01 +0100

    good project
    gsap animation learned

---
+++ Forest Cabin/index.js
@@ -0,0 +1,22 @@
+const get = element => document.getElementById(element)
+
+let open = get("menu-btn")
+let nav = get("nav")
+let exit = get("exit-btn")
+
+open.addEventListener('click', () => {
+    nav.classList.remove('close-nav')
+    nav.classList.add('open-nav')
+})
+
+exit.addEventListener('click', () => {
+    nav.classList.remove('open-nav')
+    nav.classList.add('close-nav')
+})
+
+
+var tl = gsap.timeline({defaults:{duration: 1}})
+
+tl.from(".main-copy", {y: 50, opacity: 0})
+  .to("h1 span", {clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"}, "-=.7")
+  .from("ul.featured-cabins li", {y: 50, opacity: 0, stagger: .3}, "-=.7")
